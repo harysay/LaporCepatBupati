ed09c593677676d2d539a2b22560b03f568d5975
harysay <harypoenya@gmail.com>
2025-01-24 09:48:10 +0700

    Update Server.java
    SembunyikanServer

--- app/src/main/java/id/go/kebumenkab/laporcepatbupati/handler/Server.java
+++ app/src/main/java/id/go/kebumenkab/laporcepatbupati/handler/Server.java
@@ -6,8 +6,7 @@ package id.go.kebumenkab.laporcepatbupati.handler;
 
 public class Server {
     // sesuaikan dengan nama domain hosting atau IP Address PC/laptop atau ip address emulator android 10.0.2.2
-//    public static final String URL = "https://development.kebumenkab.go.id/laporbup/index.php/api/";
-    public static final String URL = "https://laporbup.kebumenkab.go.id/index.php/api/";
+    public static final String URL = "https://url.apimu.go.id/index.php/api/";
     public static final String version = "Version 1.0.b.18042022";
     //public static final String URL = "https://skrdpasar.kebumenkab.go.id/index.php/ApiAndroid/";
     public static String TAMBAH_ADUAN = URL+"Aduan";
